421b0bceca8fd7489effeea2051661c4039cd148
rammi-dev <krystalografia@gmail.com>
2025-12-12 07:26:08 +0100

    commendout firewall

--- workloads/data_cluster_gke1/firewall.tf
+++ workloads/data_cluster_gke1/firewall.tf
@@ -1,13 +1,17 @@
-resource "google_compute_firewall" "nodeports" {
-  name    = "allow-nodeports"
-  network = "default" # GKE uses default network in our simple config
-  project = var.project_id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["30000-32767"]
-  }
-
-  source_ranges = ["0.0.0.0/0"] # Open to world (simplest for playground)
-  target_tags   = ["gke-node"]  # We need to ensure nodes have this tag
-}
+# Firewall rule for NodePort access (currently not needed - using kubectl port-forward)
+# Uncomment this if you want to expose services via NodePort for public access
+# without needing kubectl port-forward running on your laptop
+#
+# resource "google_compute_firewall" "nodeports" {
+#   name    = "allow-nodeports"
+#   network = "default" # GKE uses default network in our simple config
+#   project = var.project_id
+#
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["30000-32767"]
+#   }
+#
+#   source_ranges = ["0.0.0.0/0"] # Open to world (simplest for playground)
+#   target_tags   = ["gke-node"]  # We need to ensure nodes have this tag
+# }
